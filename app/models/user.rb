8d2b5a2254589c57ddb5e7cdbc1b7d5778cccd65
Ps1x <ps1x0209@gmail.com>
2019-03-10 20:44:06 +0200

    add all

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  after_destroy :untie_post_from_user
+
+
+  has_many :posts
+
+  def admin?
+    role == "admin"
+  end
+
+  def can_update_or_destroy_post post
+    id == post.user_id || admin?
+  end
+
+  def untie_post_from_user
+    Post.where(user_id: id).update(user_id: nil)
+  end
 end
